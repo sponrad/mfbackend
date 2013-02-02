d86fb9e8851991dc26c7b6185ed5331d8fa3c830
Conrad Frame <sponrad@gmail.com>
2013-02-01 19:02:59 -0800

    added geo library

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        self.response.write('Welcome to MF Backend! <br><br> Not much here..')
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
